719b9ea612232ab372203835b5b7cc0a06be3ac0
Hussain <iamhussainali2@gmail.com>
2023-12-02 11:10:12 +0530

    added cart functionality

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,5 +1,6 @@
 import { IProduct } from "@/types/globalTypes";
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface Icart {
     products: IProduct[];
@@ -12,8 +13,38 @@ const initialState: Icart = {
 const cartslice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        addToCart: (state, action: PayloadAction<IProduct>) => {
+
+            const existing = state.products.find(product => product._id === action.payload._id)
+
+            if (existing) {
+                existing.quantity = existing.quantity! + 1;
+            }
+            else {
+
+                state.products.push({ ...action.payload, quantity: 1 })
+            }
+
+        },
+        removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
+            const existing = state.products.find(product => product._id === action.payload._id)
+
+            if (existing) {
+                existing.quantity = existing.quantity! - 1;
+            }
+            else {
+
+                state.products.push({ ...action.payload, quantity: 1 })
+            }
+        },
+        removeFromCart: (state, action: PayloadAction<IProduct>) => {
+            state.products = state.products.filter(product => product._id !== action.payload._id)
+        }
+    }
 })
+
+export const { addToCart, removeOneFromCart, removeFromCart } = cartslice.actions;
 
 
 export default cartslice.reducer;
